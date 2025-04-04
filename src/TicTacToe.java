536b4d5e46d71b3120226d0644ce191b59d16b7b
mulandi <mulandimorris1@gmail.com>
2025-04-04 12:41:30 +0300

    adding methods

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,25 @@
-public class TicTacToe {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe  implements ActionListener {
+  TicTacToe(){
+
+  }
+  public void actionPerformed(ActionEvent e) {
+
+  }
+  //determine which player will go first
+  public void firstTurn(){
+
+  }
+  // check to see if the player has worn
+  public void check(){
+
+  }
+  public void xWins(int a, int b, int c){
+
+  }
+  public void oWins(int a, int b, int c){
+
+  }
 }
