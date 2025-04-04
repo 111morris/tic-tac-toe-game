bbdba63c3d2a50af59007552efc5e032e9496f15
mulandi <mulandimorris1@gmail.com>
2025-04-04 12:44:57 +0300

    dvpgk

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,17 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class TicTacToe  implements ActionListener {
+  Random random = new Random();
+  JFrame frame = new JFrame();
+  JPanel title_panel = new JPanel();
+  JPanel button_panel = new JPanel();
+  JLabel textField = new JLabel();
+  JButton[] buttons = new JButton[9];
+  boolean player1_turn;
+
   TicTacToe(){
 
   }
